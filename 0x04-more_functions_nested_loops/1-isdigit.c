9ce908da99f44181dce767a57ab6ea4f845b69e7
Raymond Apenteng <rayapenteng@aol.com>
2023-06-23 22:06:28 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,12 +9,9 @@
 
 int _isdigit(int c)
 {
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (isdigit(c))
+	if (c >= '0' && c <= '9')
+		return (1);
 
-			return (1);
-
-	}
-	return (0);
+	else
+		return (0);
 }
